fe27c2fcc2e45c650458eb2f4f73e35c5a0efe2a
hit9 <hit9@icloud.com>
2015-06-25 14:20:53 +0800

    Add test.h

---
+++ src/test.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
+ */
+
+#include <assert.h>
+#include <stdio.h>
+
+#ifdef __linux
+#include <mcheck.h>
+#endif
+
+struct test_case {
+    const char *name;
+    void (* fn)();
+};
+
+static void
+run_cases(struct test_case cases[])
+{
+#ifdef __linux
+    mtrace();
+#endif
+
+    int idx = 0;
+
+    while (1) {
+        struct test_case c = cases[idx];
+
+        if (c.name == NULL || c.fn == NULL)
+            break;
+
+        (c.fn)();
+        idx += 1;
+        printf("[%d] ok => %s\n", idx, c.name);
+    }
+}
