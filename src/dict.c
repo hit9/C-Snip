c6fa03c7597dda4e84550758346d9cd63d4e7241
hit9 <hit9@icloud.com>
2015-07-11 13:43:06 +0800

    Fix include

--- src/dict.c
+++ src/dict.c
@@ -3,8 +3,8 @@
  */
 
 #include <assert.h>
-#include <memory.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dict.h"
 
 /* BKDRHash algorithm. */
--- src/string.c
+++ src/string.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <memory.h>
 
 #include "errors.h"
 #include "string.h"
