ef6cd8aed063738eb74f55d4f09c784664404436
hit9 <hit9@icloud.com>
2015-09-03 21:44:25 +0800

    Use new formatter

--- src/test.h
+++ src/test.h
@@ -5,6 +5,8 @@
 #include <assert.h>
 #include <stdio.h>
 
+#include "utils.h"
+
 struct test_case {
     const char  *name;    /* test case name */
     void        (*fn)();  /* test case function */
@@ -15,15 +17,17 @@ run_cases(const char *name, struct test_case cases[])
 {
     int idx = 0;
 
-    printf("[%s]\n", name);
     while (1) {
         struct test_case c = cases[idx];
 
         if (c.name == NULL || c.fn == NULL)
             break;
 
+        double start_at = time_now();
         (c.fn)();
+        double end_at = time_now();
         idx += 1;
-        printf("  ok: %s\n", c.name);
+        printf("ok\t%s\t%s\t%.3fms\n", name, c.name,
+                end_at - start_at);
     }
 }
