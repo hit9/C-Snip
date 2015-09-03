80220061034094761321ffde258c3dad0389bf8a
hit9 <hit9@icloud.com>
2015-09-04 00:14:03 +0800

    Add `_vscnprintf` and `_scnprintf` to utils

--- src/utils.c
+++ src/utils.c
@@ -2,8 +2,51 @@
  * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
  */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <sys/time.h>
+
+#include "utils.h"
+
+/**
+ * **************** Common Utils **********************
+ */
+
+int
+_scnprintf(char *buf, size_t size, const char *fmt, ...)
+{
+    va_list args;
+    int n;
+
+    va_start(args, fmt);
+    n = _vscnprintf(buf, size, fmt, args);
+    va_end(args);
+
+    return n;
+}
+
+int
+_vscnprintf(char *buf, size_t size, const char *fmt, va_list args)
+{
+    int n;
+
+    n = vsnprintf(buf, size, fmt, args);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    if (n < (int) size) {
+        return n;
+    }
+
+    return (int)(size - 1);
+}
+
+/**
+ * ****************  Date Time Utils **********************
+ */
 
 /* Get time now in ms */
 double
--- src/utils.h
+++ src/utils.h
@@ -5,11 +5,30 @@
 #ifndef _CW_UTILS_H
 #define _CW_UTILS_H     1
 
+#include <stdarg.h>
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
+/**
+ * **************** Common Utils **********************
+ */
+
+#define MIN(a, b)   ((a) < (b) ? (a) : (b))
+#define MAX(a, b)   ((a) > (b) ? (a) : (b))
+
+int _scnprintf(char *buf, size_t size, const char *fmt, ...);
+int _vscnprintf(char *buf, size_t size, const char *fmt, va_list args);
+
+/**
+ * ****************  Date Time Utils **********************
+ */
 double time_now(void);
+
+/**
+ * ****************  IO Utils **********************
+ */
 
 #if defined(__cplusplus)
 }
