73b485ff9a8507261c387c5f7d49f99e1156ec6a
hit9 <hit9@icloud.com>
2015-11-10 16:39:39 +0800

    Add simple test for log

--- src/log_test.c
+++ src/log_test.c
@@ -9,5 +9,31 @@
 void
 case_log_open_close()
 {
-    log_open("test.log")
+    assert(log_open("test", NULL, 0) == LOG_OK);
+    assert(log_open("test", "test.log", 0) == LOG_OK);
+    assert(log_open("test", "test.log", 0) == LOG_OK);
+    assert(log_open("test", "test.log", 1024) == LOG_OK);
+    log_close();
+}
+
+void
+case_log_log()
+{
+    assert(log_open("test", "test.log", 0) == LOG_OK);
+    log_debug("test message debug");
+    log_info("test message info");
+    log_warn("test message warn");
+    log_error("test message error");
+    log_critical("test message critical");
+    log_close();
+}
+
+void
+case_log_rotate()
+{
+    assert(log_open("test", "test.log", 1024) == LOG_OK);
+    int i;
+
+    for (i = 0; i < 10; i++)
+        log_info("test message");
 }
