6187cee8a60619fa91fde01f6802a9634d194d00
hit9 <hit9@icloud.com>
2015-11-13 17:02:22 +0800

    Minor updates on event example

--- src/event_example.c
+++ src/event_example.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "event.h"
 
@@ -12,16 +13,28 @@ echo(struct event_loop *loop, int fd, int mask, void *data)
 
     if (fd == STDIN_FILENO) {
         scanf("%s", s);
-        printf("your input is: %s\n", s);
+
+        if (strcmp(s, "exit") == 0) {
+            /* stop the event loop on message "exit" */
+            printf("bye!\n");
+            event_loop_stop(loop);
+        } else {
+            printf("your input is: %s\n", s);
+        }
     }
 }
 
 int
 main(int argc, const char *argv[])
 {
-    struct event_loop *loop = event_loop_new(1024);
+    /* allocate a new event loop with events number limitation 1024 */
+    struct event_loop *loop = event_loop(1024);
+    /* call `echo()` when stdin is readable (on data coming in) */
     event_add_in(loop, STDIN_FILENO, &echo, NULL);
+    /* start event loop */
+    printf("start event loop, type 'exit' to exit.. \n");
     event_loop_start(loop, -1);
+    /* free event loop */
     event_loop_free(loop);
     return 0;
 }
