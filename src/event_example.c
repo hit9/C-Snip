7dce23efa6b502fdee2d63222f7fe9ee4e250695
hit9 <hit9@icloud.com>
2015-11-13 16:53:23 +0800

    Add event example

---
+++ src/event_example.c
@@ -0,0 +1,27 @@
+// cc event_example.c event.c
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "event.h"
+
+void
+echo(struct event_loop *loop, int fd, int mask, void *data)
+{
+    char s[256];
+
+    if (fd == STDIN_FILENO) {
+        scanf("%s", s);
+        printf("your input is: %s\n", s);
+    }
+}
+
+int
+main(int argc, const char *argv[])
+{
+    struct event_loop *loop = event_loop_new(1024);
+    event_add_in(loop, STDIN_FILENO, &echo, NULL);
+    event_loop_start(loop, -1);
+    event_loop_free(loop);
+    return 0;
+}
