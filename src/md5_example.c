775571c0992a5098c0c4738de18a5d69c8612719
hit9 <hit9@icloud.com>
2015-11-13 22:37:02 +0800

    Add example for md5

---
+++ src/md5_example.c
@@ -0,0 +1,20 @@
+// cc md5_example.c md5.c
+
+#include <stdio.h>
+#include <string.h>
+#include "md5.h"
+
+int main(int argc, const char *argv[])
+{
+    char *key = "key";
+    unsigned long len = strlen(key);
+    /* get hashing number by key */
+    printf("md5 hash for 'key': %u\n", hash_md5(key, len));
+    /* get md5 signatures (16) */
+    unsigned char results[16];
+    md5_signature((unsigned char *)key, len, results);
+    int i;
+    for (i = 0; i < 16; i++)
+        printf("%d", results[i]);
+    return 0;
+}
