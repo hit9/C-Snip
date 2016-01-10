fad2a3cf8fd4c75268dc46bbc7dddb48baeddf14
hit9 <hit9@icloud.com>
2016-01-10 12:28:49 +0800

    Use clock_gettime if have (linux/bsd but not apple)

--- src/datetime.c
+++ src/datetime.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2015, Chao Wang <hit9@icloud.com>
  */
 
+#include <assert.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -9,7 +10,14 @@
 double
 datetime_stamp_now(void)
 {
+#if defined CLOCK_REALTIME
+    struct timespec ts;
+    int rc = clock_gettime(CLOCK_REALTIME, &ts);
+    assert(rc == 0);
+    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000.0;
+#else
     struct timeval tv;
     gettimeofday(&tv, NULL);
     return (1000000 * tv.tv_sec + tv.tv_usec) / 1000.0;
+#endif
 }
